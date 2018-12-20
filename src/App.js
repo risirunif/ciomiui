5d32856957b901928d7e5d39fba015e330c0d89e
AaronFlower <haojunzhan@gmail.com>
2018-12-20 20:39:33 +0800

    init shippy-ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {component} from 'react'
+import './App.css'
+import CreateConsignment from './CreateConsignment'
+import Authenticate from './Authenticate'
+
+
+class App extends component {
+
+    state = {
+      err: null,
+      authenticated: false,
+    }
+    
+}
